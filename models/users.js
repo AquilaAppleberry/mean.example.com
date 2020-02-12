29f9bb8c7843f27e325fe46dc4f1bdac20678d4f
Aquila Appleberry <AquilaAppleberry@yahoo.com>
2020-02-12 02:34:20 -0600

    Initial Commit

---
+++ models/users.js
@@ -0,0 +1,58 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var uniqueValidator = require('mongoose-unique-validator');
+var passportLocalMongoose = require('passport-local-mongoose'); 
+
+//Create a schema
+var Users = new Schema({
+  email: {
+    type: String,
+    required: [true, 'Please enter an email'],
+    unique: [true, 'Email must be unique']
+  },
+  username: {
+    type: String,
+    required: [true, 'Please enter a username'],
+    unique: [true, 'Usernames must be unique']
+  },
+  first_name: String,
+  last_name: String,
+  admin: {
+    type: Boolean,
+    default: false
+  },
+  hash: {
+    type: String,
+    required: [
+      true,   
+      'There was a problem creating your password'
+    ]
+  },
+  salt: {
+    type: String,
+    required: [
+      true, 
+      'There was a problem creating your password'
+    ]
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  modified: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+//Add unique validation properties to the model
+Users.plugin(uniqueValidator);
+
+Users.pre('save', function(next){
+  this.modified = new Date().toISOString();
+  next();
+});
+
+Users.plugin(passportLocalMongoose);
+
+module.exports  = mongoose.model('Users', Users);
---
+++ routes/users.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/app', function(req, res, next) {
+  res.render('users/app', { title: 'User Management' });
+});
+
+module.exports = router;
