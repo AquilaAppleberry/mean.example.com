cf75360d0034202441204f8ce28dd946a9fe6c8d
Aquila Appleberry <AquilaAppleberry@yahoo.com>
2020-02-24 20:57:46 -0600

    Add logout methods

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -60,14 +60,13 @@ router.post('/login', function(req, res, next) {
     });
   })(req, res, next);
 });
+
 router.delete('/logout', function(req, res){
-    req.session.destroy(function (err) {
-      if(err){
-        return res.json({success: 'false'});
-      }else{
-        return res.json({success: 'true'});
-      }
-    });
-  });
+  req.logout();
+  if(!req.session.passport.user){
+    return res.json({success: 'true'});
+  }
+  return res.json({success: 'false'});
+});
 
 module.exports = router;
